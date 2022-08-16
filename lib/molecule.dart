5d439570c664c657f346d759bca5a3c9fa1536c5
Gerson Victor <gerson.vfs@gmail.com>
2022-08-16 20:37:01 -0300

    implement molecule class

--- lib/molecule.dart
+++ lib/molecule.dart
@@ -1,33 +1,68 @@
-import 'package:app/errors/invalid_symbol.dart';
+import 'package:app/atom.dart';
 
-class Molecule {
-  late String _formula;
+class Molecule implements Comparable<Molecule> {
+  late Map<Atom, int> _atoms;
 
   Molecule(String formula) {
     this.formula = formula;
   }
 
   set formula(String formula) {
-    _formula = formula;
-
-    if (!_isValidFormula(formula)) {
-      throw InvalidSymbolError(formula);
-    }
+    _atoms = split(formula);
   }
 
   String get formula {
-    return _formula;
+    return join(_atoms);
   }
 
   int get weight {
-    return _getAtomWeight("");
+    throw UnimplementedError();
   }
 
-  bool _isValidFormula(String formula) {
-    return true;
+  @override
+  int compareTo(Molecule other) {
+    throw UnimplementedError();
   }
 
-  int _getAtomWeight(String symbol) {
-    return 0;
+  static Map<Atom, int> split(String formula) {
+    Map<Atom, int> atoms = {};
+
+    for (var atom in formula.split("")) {
+      if (int.tryParse(atom) != null) {
+        atoms[atoms.entries.last.key] = int.parse(atoms.entries.last.value == 0
+            ? atom
+            : atoms.entries.last.value.toString() + atom);
+      } else {
+        if (atoms.isNotEmpty) {
+          if (atoms.entries.last.value == 0) {
+            atoms[atoms.entries.last.key] = 1;
+          }
+        }
+
+        atoms[Atom(atom)] = 0;
+      }
+    }
+
+    if (atoms.isNotEmpty) {
+      if (atoms.entries.last.value == 0) {
+        atoms[atoms.entries.last.key] = 1;
+      }
+    }
+
+    return atoms;
+  }
+
+  static String join(Map<Atom, int> atoms) {
+    String formula = '';
+
+    for (var atom in atoms.entries) {
+      formula += atom.key.symbol;
+
+      if (atom.value > 1) {
+        formula += atom.value.toString();
+      }
+    }
+
+    return formula;
   }
 }
