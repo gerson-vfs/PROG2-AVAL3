7960c6e442230d5b909992010b92ee26cc5b7cdb
Gerson Victor <gerson.vfs@gmail.com>
2022-08-16 20:37:25 -0300

    implement element class

---
+++ lib/element.dart
@@ -0,0 +1,8 @@
+class Element {
+  String symbol;
+  String portugueseName;
+  String latimName;
+  int atomicNumber;
+
+  Element(this.symbol, this.portugueseName, this.latimName, this.atomicNumber);
+}
