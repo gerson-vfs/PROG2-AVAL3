f5c7a966973f14f498e9a423ccac9f9e82aa71ab
Gerson Victor <gerson.vfs@gmail.com>
2022-08-16 21:08:44 -0300

    get elements from singleton

--- lib/atom.dart
+++ lib/atom.dart
@@ -1,127 +1,7 @@
+import 'package:app/Elements.dart';
 import 'package:app/errors/invalid_symbol.dart';
 
 class Atom {
-  static final acceptedSymbols = [
-    'H',
-    'He',
-    'Li',
-    'Be',
-    'B',
-    'C',
-    'N',
-    'O',
-    'F',
-    'Ne',
-    'Na',
-    'Mg',
-    'Al',
-    'Si',
-    'P',
-    'S',
-    'Cl',
-    'Ar',
-    'K',
-    'Ca',
-    'Sc',
-    'Ti',
-    'V',
-    'Cr',
-    'Mn',
-    'Fe',
-    'Co',
-    'Ni',
-    'Cu',
-    'Zn',
-    'Ga',
-    'Ge',
-    'As',
-    'Se',
-    'Br',
-    'Kr',
-    'Rb',
-    'Sr',
-    'Y',
-    'Zr',
-    'Nb',
-    'Mo',
-    'Tc',
-    'Ru',
-    'Rh',
-    'Pd',
-    'Ag',
-    'Cd',
-    'In',
-    'Sn',
-    'Sb',
-    'Te',
-    'I',
-    'Xe',
-    'Cs',
-    'Ba',
-    'La',
-    'Ce',
-    'Pr',
-    'Nd',
-    'Pm',
-    'Sm',
-    'Eu',
-    'Gd',
-    'Tb',
-    'Dy',
-    'Ho',
-    'Er',
-    'Tm',
-    'Yb',
-    'Lu',
-    'Hf',
-    'Ta',
-    'W',
-    'Re',
-    'Os',
-    'Ir',
-    'Pt',
-    'Au',
-    'Hg',
-    'Tl',
-    'Pb',
-    'Bi',
-    'Po',
-    'At',
-    'Rn',
-    'Fr',
-    'Ra',
-    'Ac',
-    'Th',
-    'Pa',
-    'U',
-    'Np',
-    'Pu',
-    'Am',
-    'Cm',
-    'Bk',
-    'Cf',
-    'Es',
-    'Fm',
-    'Md',
-    'No',
-    'Lr',
-    'Rf',
-    'Db',
-    'Sg',
-    'Bh',
-    'Hs',
-    'Mt',
-    'Ds',
-    'Rg',
-    'Cn',
-    'Nh',
-    'Fl',
-    'Mc',
-    'Lv',
-    'Ts',
-    'Og'
-  ];
-
   final String symbol;
   Atom(this.symbol) {
     if (!isValid(symbol)) {
@@ -130,7 +10,15 @@ class Atom {
   }
 
   static bool isValid(String symbol) {
-    return acceptedSymbols.contains(symbol);
+    final elements = Elements();
+
+    for (var element in elements.elements) {
+      if (element.symbol == symbol) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   @override
